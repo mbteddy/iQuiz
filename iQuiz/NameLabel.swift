a6ea13daf538947347f26474f30f7e03bd5aae8d
Teddy Pappas <mbteddy@verizon.net>
2015-11-05 14:46:38 -0800

    first commit

---
+++ iQuiz/NameLabel.swift
@@ -0,0 +1,21 @@
+//
+//  NameLabel.swift
+//  iQuiz
+//
+//  Created by Teddy Pappas on 10/30/15.
+//  Copyright © 2015 Teddy Pappas. All rights reserved.
+//
+
+import UIKit
+
+class NameLabel: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ iQuiz/Subject.swift
@@ -0,0 +1,9 @@
+//
+//  Subject.swift
+//  iQuiz
+//
+//  Created by Teddy Pappas on 10/30/15.
+//  Copyright © 2015 Teddy Pappas. All rights reserved.
+//
+
+import Foundation
